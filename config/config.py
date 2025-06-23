a1e93853632b6ea97763d362d4d690bd5e46b2ca
shelby09021 <wickjohn43213@gmail.com>
2025-06-23 09:32:08 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -108,27 +108,27 @@ clean = {}
 autoclean = []
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/d593c6064ff7657d0c714.jpg"
+    "START_IMG_URL", "https://graph.org/file/40c5127b025a3956f6425-210a5f197664b1a194.jpg"
 )
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "assets/Ping.jpeg",
+    "https://graph.org/file/40c5127b025a3956f6425-210a5f197664b1a194.jpg",
 )
 
 PLAYLIST_IMG_URL = getenv(
     "PLAYLIST_IMG_URL",
-    "assets/Playlist.jpeg",
+    "https://graph.org/file/40c5127b025a3956f6425-210a5f197664b1a194.jpg",
 )
 
 GLOBAL_IMG_URL = getenv(
     "GLOBAL_IMG_URL",
-    "assets/Global.jpeg",
+    "https://graph.org/file/40c5127b025a3956f6425-210a5f197664b1a194.jpg",
 )
 
 STATS_IMG_URL = getenv(
     "STATS_IMG_URL",
-    "assets/Stats.jpeg",
+    "https://graph.org/file/40c5127b025a3956f6425-210a5f197664b1a194.jpg",
 )
 
 TELEGRAM_AUDIO_URL = getenv(
